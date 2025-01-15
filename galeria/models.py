124343ec140f434cf7707e7a07e4c2cf6eb484b9
Nathalya <nathalya.gsn@gmail.com>
2025-01-15 11:33:27 -0300

    publicacao

--- galeria/models.py
+++ galeria/models.py
@@ -19,5 +19,7 @@ class Fotografia(models.Model):
 
     foto = models.CharField(max_length=100, null=False, blank=False) 
 
+    publicada = models.BooleanField(default=False)
+
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
